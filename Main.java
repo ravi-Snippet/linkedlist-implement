a0c23ba396f0c962c3c832565405d2130d681090
ravi-Snippet <raviroshan1994@gmail.com>
2024-03-08 20:46:43 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+import pack.*;
+public class Main
+ {
+	 public static void main( String args[])
+	 {
+		 LinkedList list=new LinkedList();
+		 list.insert(12);
+		 list.insert(13);
+		 list.insert(14);
+		 list.insert(52);
+		 list.show();
+	 }
+ }
